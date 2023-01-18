4e0a5150312f72873f8e4afc3a2e19e8b4c978a2
minhuanli <minhuanli@g.harvard.edu>
2023-01-18 15:58:27 -0500

    remove deprecated codes

--- SFC_Torch/voxel.py
+++ SFC_Torch/voxel.py
@@ -4,60 +4,6 @@ import numpy as np
 from .utils import try_gpu
 from .symmetry import asu2p1_torch
 
-# def voxelvalue_torch_asu(unitcell_grid_center_orth, atom_pos_orth,
-#                       frac2orth_tensor, orth2frac_tensor,
-#                       vdw_rad_tensor,
-#                       s=10., binary=True, cutoff=0.1):
-#     '''
-#     Differentiably render atom coordinates into real space grid map value
-#     Va(d, ra) = 1 / (1 + exp(s*(d-ra)))
-
-#     Without applying symmetry operations to the model
-
-#     Reference:
-#     Orlando, Gabriele, et al. "PyUUL provides an interface between biological 
-#     structures and deep learning algorithms." Nature communications 13.1 (2022)
-
-#     Parameters
-#     ----------
-#     unitcell_grid_center_orth: tensor, [N_grid, 3]
-#         Cartesian coordinates of real space unitcell grid center
-
-#     atom_pos_orth: tensor, [N_atom, 3]
-#         cartesian coordinates of model atoms you want to render, in an asu
-
-#     frac2orth_tensor: tensor, [3,3]
-#         fractionalize matrix
-
-#     vdw_rad_tensor: tensor, [N_atom, ]
-#         van de waals radius of atoms in the model accordingly
-
-#     s: float, default 10.0
-#         steepness parameter in the sigmoid function
-
-#     binary: binary, default True
-#         whether convert the map value to 0/1
-
-#     cutoff: float, default 0.1
-#         cutoff value to convert to binary. 1 if > cutoff, 0 otherwise.
-
-#     Returns
-#     -------
-#     voxel_value, tensor [N_grid,]
-#     '''
-#     atom_pos_frac = torch.tensordot(atom_pos_orth, orth2frac_tensor.T, 1)
-#     atom_pos_frac_incell = atom_pos_frac - torch.floor(atom_pos_frac)
-#     atom_pos_orth_incell = torch.tensordot(atom_pos_frac_incell, frac2orth_tensor.T, 1)
-#     voxel2atom_dist = torch.sqrt(torch.sum(torch.square(unitcell_grid_center_orth[:, None, :] - atom_pos_orth_incell[None, ...]),
-#                                             dim=-1))
-#     sigmoid_value = 1./(1.+torch.exp(s*(voxel2atom_dist - vdw_rad_tensor)))
-#     voxel_value = torch.sum(sigmoid_value, dim=-1)
-#     if binary:
-#         return torch.where(voxel_value > cutoff, 1.0, 0.0)
-#     else:
-#         return voxel_value
-
-
 def voxelvalue_torch_p1(unitcell_grid_center_orth, atom_pos_orth, unit_cell, space_group, vdw_rad_tensor,
                         s=10., binary=True, cutoff=0.1):
     '''
